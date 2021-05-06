30841eab269896d41eefe6027b505415c8742745
Flamefire <Flamefire@users.noreply.github.com>
2021-05-06 20:18:41 +0200

    Add missing copyright and license notices

--- src/md5sum.cpp
+++ src/md5sum.cpp
@@ -1,52 +1,24 @@
-/*
- * md5sum.c - Generate/check MD5 Message Digests
- *
- * Compile and link with md5.c.  If you don't have getopt() in your library
- * also include getopt.c.  For MSDOS you can also link with the wildcard
- * initialization function (wildargs.obj for Turbo C and setargv.obj for MSC)
- * so that you can use wildcards on the commandline.
- *
- * Make sure that you compile with -DHIGHFIRST if you are on a big-endian
- * system. Here are some examples of correct MD5 sums:
- *
- * % echo "The meeting last week was swell." | md5sum
- * 050f3905211cddf36107ffc361c23e3d
- *
- * % echo 'There is $1500 in the blue box.' | md5sum
- * 05f8cfc03f4e58cbee731aa4a14b3f03
- *
- * If you get anything else than this, then you've done something wrong. ;)
- *
- * Written March 1993 by Branko Lankester
- * Modified June 1993 by Colin Plumb for altered md5.c.
- */
+// Copyright (C) 2005 - 2021 Settlers Freaks <sf-team at siedler25.org>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include "md5sum.h"
-#include "md5.h"
+#include "s25util/md5.hpp"
 #include <array>
-#include <cstdio>
-#include <iomanip>
-#include <sstream>
-#include <string>
+#include <cstdint>
 
 int md5file(FILE* fp, std::string& digest)
 {
     if(!fp)
         return -1;
-    std::array<unsigned char, 16> d;
-    std::array<unsigned char, 1024> buf;
-    md5Context ctx;
+    std::array<uint8_t, 1024> buf;
+    s25util::md5 md5("");
 
-    md5Init(&ctx);
     size_t n;
-    while((n = fread(buf.data(), 1, sizeof(buf), fp)) > 0)
-        md5Update(&ctx, buf.data(), n);
-    md5Final(&ctx, d);
+    while((n = fread(buf.data(), 1, buf.size(), fp)) > 0)
+        md5.process(buf.data(), n, true);
 
-    std::stringstream s;
-    for(unsigned char i : d)
-        s << std::hex << std::setiosflags(std::ios::fixed) << std::setfill('0') << std::setw(2) << static_cast<int>(i);
-
-    digest = s.str();
+    digest = md5.toString();
 
     if(ferror(fp))
         return -1;
--- src/s25update.cpp
+++ src/s25update.cpp
@@ -1,22 +1,10 @@
-// Copyright (c) 2005 - 2017 Settlers Freaks (sf-team at siedler25.org)
+// Copyright (C) 2005 - 2021 Settlers Freaks (sf-team at siedler25.org)
 //
-// This file is part of Return To The Roots.
-//
-// Return To The Roots is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
-//
-// Return To The Roots is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Return To The Roots. If not, see <http://www.gnu.org/licenses/>.
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "s25update.h" // IWYU pragma: keep
 #include "md5sum.h"
+#include "s25util/warningSuppression.h"
 #include <boost/filesystem.hpp>
 #include <boost/nowide/cstdio.hpp>
 #include <boost/nowide/fstream.hpp>
@@ -367,6 +355,9 @@ bool runAsAdmin(int argc, char* argv[])
     DWORD dwError = GetLastError();
     if(dwError == ERROR_CANCELLED)
         bnw::cerr << "You refused to elevate - cannot update" << std::endl;
+#else
+    RTTR_UNUSED(argc);
+    RTTR_UNUSED(argv);
 #endif
     return false;
 }
